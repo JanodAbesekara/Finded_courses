6cfce8da651d16fef52b759900370ac3ab2fd889
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-09-24 22:22:07 +0530

    fix

--- findcourse/src/main/java/com/FindCourse/findcourse/Config/SecurityConfig.java
+++ findcourse/src/main/java/com/FindCourse/findcourse/Config/SecurityConfig.java
@@ -5,32 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http    .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/", "/gouth","api/V1/addFeedback" ,"/api/V1/getuserdetails/**" ).permitAll()
-                                .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2Login ->
-                        oauth2Login
-                                .defaultSuccessUrl("http://localhost:3000/Projile", true)  // Redirect to frontend on success
-                );
+                                .anyRequest().authenticated())
+               // .formLogin(form-> form.defaultSuccessUrl("/greet", true));
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/Projile", true));
         return http.build();
     }
 
--- findcourse/src/main/java/com/FindCourse/findcourse/Controller/UserController.java
+++ findcourse/src/main/java/com/FindCourse/findcourse/Controller/UserController.java
@@ -1,29 +1,37 @@
 package com.FindCourse.findcourse.Controller;
 
-
 import com.FindCourse.findcourse.Model.FeedBacks;
 import com.FindCourse.findcourse.Services.UserServices;
 import com.FindCourse.findcourse.dto.AddFeedBacksDTO;
 import com.FindCourse.findcourse.dto.UserDTO;
+
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+
 
 @RequestMapping(value = "api/V1")
 @CrossOrigin
 @RestController
 public class UserController {
 
-
     @Autowired
     private UserServices userServices;
+
+    @GetMapping("/User-info")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttributes();
+    }
 
     // Add feedback for a user
     @PostMapping("/addFeedback")
     public FeedBacks addFeedback(@RequestBody AddFeedBacksDTO feedbackDTO) {
         return userServices.saveFeedback(feedbackDTO.getUseremail(), feedbackDTO.getFeedback());
     }
-
 
     @GetMapping("/getuserdetails")
     public ResponseEntity<UserDTO> getUserByEmail(@RequestParam String email) {
